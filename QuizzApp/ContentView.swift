a8ef3ee7d67ae48b4f1c4af73f8d034cd64afa3d
Tyago <santiagox46@hotmail.com>
2025-04-25 04:19:00 -0400

    Initial Commit

---
+++ QuizzApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  QuizzApp
+//
+//  Created by Tyago on 2025-04-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!!!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ QuizzApp/QuizzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzAppApp.swift
+//  QuizzApp
+//
+//  Created by Tyago on 2025-04-25.
+//
+
+import SwiftUI
+
+@main
+struct QuizzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
